2e74f23fa4a4c2afda4361049f6ff01606ddb02a
raulagras <raul.agras04@gmail.com>
2024-11-07 17:45:07 +0100

    Arreglo de warning

--- query_cassandra.py
+++ query_cassandra.py
@@ -5,11 +5,20 @@ import time
 cluster = Cluster(['localhost'])
 session = cluster.connect('testkeyspace')
 
+# Definir la consulta
+query = "SELECT AVG(Calificacion) FROM estudiantes"
+print(f"Ejecutando consulta en Cassandra: {query}")
+
 # Medir el tiempo de ejecución de la consulta
 start_time = time.time()
-rows = session.execute("SELECT AVG(Calificacion) FROM estudiantes")
-avg_cassandra = rows[0][0]
+rows = session.execute(query)
+
+# Usar 'one()' para obtener la primera fila (única fila) y acceder al valor
+avg_cassandra = rows.one()[0]  # 'one()' devuelve la primera fila, y [0] es el primer valor de esa fila
 end_time = time.time()
 
+session.shutdown()
+
+# Mostrar el resultado
 print(f"Promedio de Calificación en Cassandra: {avg_cassandra}")
 print(f"Tiempo de ejecución en Cassandra: {end_time - start_time} segundos")
